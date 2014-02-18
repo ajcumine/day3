4215e0cbd0999244701e85b0917e6ae830f3ca68
Andrew <aj.cumine@btinternet.com>
2014-02-18 15:26:09 +0000

    update makers.rb with interpolation

--- makers.rb
+++ makers.rb
@@ -1,7 +1,7 @@
 # Assign the string "Makers" to a variable and print it out
 people = "Makers"
-puts "The people variable is currently: " + people
+puts "The people variable is currently: #{people}"
 
 # Change the variable to "Coders" and print it again
 people = "Coders"
-puts "The people variable is currently: " + people
+puts "The people variable is currently: #{people}"
